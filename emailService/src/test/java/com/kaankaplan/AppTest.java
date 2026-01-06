10c01da5f44fd006f2bf1b0cfc220bd7a3d8424b
norelimane <168724221+norelimane@users.noreply.github.com>
2026-01-06 01:26:15 +0100

    Initial commit

---
+++ emailService/src/test/java/com/kaankaplan/AppTest.java
@@ -0,0 +1,21 @@
+package com.kaankaplan;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
